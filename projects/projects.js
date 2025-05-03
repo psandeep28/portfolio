dd3285c1a592f412745c7e952f7af667df12b67e
Parth Sandeep <psandeep@ucsd.edu>
2025-05-03 01:13:58 -0700

    start of legend

--- projects/projects.js
+++ projects/projects.js
@@ -20,23 +20,25 @@ for (let project of projects) {
   yearCounts[year] = (yearCounts[year] || 0) + 1;
 }
 
-// Convert to arrays for D3
-const years = Object.keys(yearCounts);
-const data = Object.values(yearCounts);
+// Transform into D3-friendly format
+let data = Object.entries(yearCounts).map(([year, count]) => ({
+  label: year,
+  value: count
+}));
 
 // Arc generator
 const arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
 // Pie slice generator
-const sliceGenerator = d3.pie();
+const sliceGenerator = d3.pie().value(d => d.value);
 
 // Generate angle data
 const arcData = sliceGenerator(data);
 
-// Color scale (more flexible than a fixed list)
+// Color scale
 const colors = d3.scaleOrdinal(d3.schemeTableau10);
 
-// Draw each arc
+// Draw pie chart
 arcData.forEach((d, idx) => {
   d3.select('#projects-pie-plot')
     .append('path')
@@ -44,4 +46,15 @@ arcData.forEach((d, idx) => {
     .attr('fill', colors(idx))
     .attr('stroke', '#fff')
     .attr('stroke-width', 0.5);
+});
+
+// ✅ Add legend
+let legend = d3.select('.legend');
+
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('class', 'legend-item')
+    .attr('style', `--color:${colors(idx)}`)
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
