997451b2ad8cc373f282e37a883acfb95f5a48e1
Parth Sandeep <psandeep@ucsd.edu>
2025-05-09 12:08:23 -0700

    meta main js

---
+++ Meta/main.js
@@ -0,0 +1,65 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+async function loadData() {
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: +row.line,
+    depth: +row.depth,
+    length: +row.length,
+    date: new Date(row.date + 'T00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
+  return data;
+}
+
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/YOUR_REPO/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        enumerable: false,
+      });
+      return ret;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  dl.append('dt').text('Total commits');
+  dl.append('dd').text(commits.length);
+
+  dl.append('dt').text('Number of files');
+  dl.append('dd').text(d3.groups(data, d => d.file).length);
+
+  dl.append('dt').text('Max depth');
+  dl.append('dd').text(d3.max(data, d => d.depth));
+
+  dl.append('dt').text('Average line length');
+  dl.append('dd').text(d3.mean(data, d => d.length).toFixed(2));
+
+  const longestLine = d3.greatest(data, d => d.length);
+  dl.append('dt').text('Longest line');
+  dl.append('dd').text(longestLine?.text || 'N/A');
+}
+
+const data = await loadData();
+const commits = processCommits(data);
+renderCommitInfo(data, commits);
