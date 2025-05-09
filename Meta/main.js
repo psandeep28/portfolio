3143d8591aeae448378fd7116130e41df28c73e3
Parth Sandeep <psandeep@ucsd.edu>
2025-05-09 14:43:33 -0700

    added scatter plot

--- Meta/main.js
+++ Meta/main.js
@@ -60,8 +60,70 @@ function renderCommitInfo(data, commits) {
   dl.append('dd').text(longestLine?.text || 'N/A');
 }
 
-const data = await loadData();
-const commits = processCommits(data);
-console.log(commits);
+function renderScatterPlot(data, commits) {
+  const width = 1000;
+  const height = 600;
+  const margin = { top: 10, right: 10, bottom: 30, left: 50 };
 
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom,
+  };
+
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .style("overflow", "visible");
+
+  // Scales
+  const xScale = d3.scaleTime()
+    .domain(d3.extent(commits, (d) => d.datetime))
+    .range([usableArea.left, usableArea.right])
+    .nice();
+
+  const yScale = d3.scaleLinear()
+    .domain([0, 24])
+    .range([usableArea.bottom, usableArea.top]);
+
+  // ✅ Gridlines BEFORE the axes
+  svg.append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+  // ✅ Dots
+  const dots = svg.append("g").attr("class", "dots");
+
+  dots.selectAll("circle")
+    .data(commits)
+    .join("circle")
+    .attr("cx", (d) => xScale(d.datetime))
+    .attr("cy", (d) => yScale(d.hourFrac))
+    .attr("r", 4)
+    .attr("fill", "steelblue");
+
+  // ✅ Axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+  svg.append("g")
+    .attr("transform", `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("transform", `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+}
+
+
+let data = await loadData();
+let commits = processCommits(data);
+console.log(commits);
 renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);v
